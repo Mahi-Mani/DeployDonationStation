69f25662d949f30b2a8d5d8b405febcab057213a
Mahi Mani <m.mahishamani@gmail.com>
2019-11-12 11:02:04 -0800

    Deleted react application

--- create-react-express/mongodb_models/index.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  DonatorCard: require("./donatorcard")
-};
--- create-react-express/routes/reactApiRoutes.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const cardsController = require("../controllers/cardsController");
-
-console.log("Inside react api routes");
-
-router.route("/allCards")
-  .get(cardsController.findAll);
-
-module.exports = router;
